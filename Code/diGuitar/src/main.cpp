8a857c439969ef31ca368a5c6f706a3d64c96fc5
GustavAbrahamsson <gustav.abrahamsson@outlook.com>
2022-04-04 00:32:49 +0200

    Volume is random

--- Code/diGuitar/src/main.cpp
+++ Code/diGuitar/src/main.cpp
@@ -1,73 +1,106 @@
 #include <Arduino.h>
 #include "AudioTools.h"
+#include "Audiolibs/AudioKit.h"
 
 using namespace audio_tools;
 
-#define volume 150
+#define MAX_VOLUME 150
+#define DEFAULT_BOOST_VOL 1.0
 
 #define pot1Pin A4
 
 uint32_t pot1Value = 0;
 
-typedef int16_t sound_t;                                   // sound will be represented as int16_t (with 2 bytes)
-uint16_t sample_rate=44100;
-uint8_t channels = 2;                                      // The stream will have 2 channels 
-SineWaveGenerator<sound_t> sineWave(volume);                // subclass of SoundGenerator with max amplitude of 32000
-SquareWaveGenerator<sound_t> squareWaveE(volume);
-SquareWaveGenerator<sound_t> squareWaveA(volume);
-SquareWaveGenerator<sound_t> squareWaveD(volume);
+typedef int16_t sound_t;
+uint16_t sample_rate = 44100;
+uint8_t channels = 2;
 
-GeneratedSoundStream<sound_t> sound(sineWave);   // Stream generated from sine wave
+SquareWaveGenerator<sound_t> squareWaveE(MAX_VOLUME);
+SquareWaveGenerator<sound_t> squareWaveA(MAX_VOLUME);
+SquareWaveGenerator<sound_t> squareWaveD(MAX_VOLUME);
+
+//GeneratorMixer<SineWaveGenerator<int16_t>> mixer();
+
+//GeneratedSoundStream<sound_t> sound(sineWave);
 GeneratedSoundStream<sound_t> soundE(squareWaveE);
 GeneratedSoundStream<sound_t> soundA(squareWaveA);
 GeneratedSoundStream<sound_t> soundD(squareWaveD);
-I2SStream out; 
-StreamCopy copier(out, sound);                          // copies sound into i2s
-StreamCopy copierE(out, soundE);
+//StreamCopy copierE(out, soundE);
 //StreamCopy copierA(out, soundA);
-StreamCopy copierD(out, soundD);      
+//StreamCopy copierD(out, soundD);
 
-const uint16_t E_STRING_FREQS[] = {82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262};
-const uint16_t A_STRING_FREQS[] = {110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349};
-const uint16_t D_STRING_FREQS[] = {147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466};
-const uint16_t G_STRING_FREQS[] = {196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622};
-const uint16_t B_STRING_FREQS[] = {247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784};
-const uint16_t THIN_E_STRING_FREQS[] = {349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 933, 1047};
+I2SStream i2s;
 
+SineWaveGenerator<sound_t> sineWave(MAX_VOLUME);
+AudioEffects<SineWaveGenerator<sound_t>> effects(sineWave);
+GeneratedSoundStream<sound_t> effectSound(effects);
+Boost boost(DEFAULT_BOOST_VOL);      
 
+StreamCopy copier(i2s, effectSound);    
+
+StreamCopy copier2(i2s, soundE);
+
+float volFactor = 1.0;
+ConverterScaler<float> volume(volFactor, 0, MAX_VOLUME);
+
+const uint16_t E_STRING_FREQS[] =       {82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262};
+const uint16_t A_STRING_FREQS[] =       {110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349};
+const uint16_t D_STRING_FREQS[] =       {147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466};
+const uint16_t G_STRING_FREQS[] =       {196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622};
+const uint16_t B_STRING_FREQS[] =       {247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784};
+const uint16_t THIN_E_STRING_FREQS[] =  {349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 933, 1047};
+
+const uint16_t ALL_FREQS[] =            {73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 
+                                        262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 
+                                        880, 932, 933, 1047};
 uint8_t noteIndex = 0;
 
-// Arduino Setup
+String tuning = "E-standard";   // "Half step down", "D-standard"
+
 void setup(void) {
-  // Open Serial 
   Serial.begin(115200);
   pinMode(pot1Pin, INPUT);
-
-  // start the bluetooth
   Serial.println("starting I2S...");
-  I2SConfig config = out.defaultConfig(TX_MODE);
+  I2SConfig config = i2s.defaultConfig(TX_MODE);
   config.sample_rate = sample_rate; 
   config.channels = channels;
   config.bits_per_sample = 16;
-  out.begin(config);
+  i2s.begin(config);
 
-  // Setup sine wave
-  //squareWave.begin(channels, sample_rate, 500);
+  sineWave.begin(channels, sample_rate, 500);
+  sineWave.setFrequency(100);
+  boost.setActive(1);
+  boost.setVolume(0.5);
+  effects.addEffect(boost);
+  effectSound.begin(config);
+  
+  squareWaveE.begin(channels, sample_rate, 100);
 }
-
-// Arduino loop - copy sound to out 
+ 
 void loop() {
   pot1Value = analogRead(pot1Pin);
-  Serial.println(pot1Value);
+  //Serial.println(pot1Value);
   noteIndex = map(pot1Value, 0, 4024, 0, 20);
-  Serial.println(noteIndex);
+  volFactor = (double)pot1Value/4050;
+  //Serial.println(volFactor);
+
+  //Serial.println(boost.volume());
+  //squareWaveE.begin(channels, sample_rate, 100);
+
+
+  
+  //if(abs(boost.volume() - volFactor) > 0.02) boost.setVolume(volFactor);
+  if(abs(volume.factor() - volFactor) > 0.05) volume.setFactor(volFactor);
+
+  //Serial.println(noteIndex);
   //squareWave.begin(channels, sample_rate, E_STRING_FREQS[noteIndex]);
 
-  squareWaveE.begin(channels, sample_rate, E_STRING_FREQS[noteIndex]);
-  squareWaveA.begin(channels, sample_rate, A_STRING_FREQS[noteIndex+2]);
+  //squareWaveE.begin(channels, sample_rate, E_STRING_FREQS[noteIndex]);
+  //squareWaveA.begin(channels, sample_rate, A_STRING_FREQS[noteIndex+2]);
   //squareWaveD.begin(channels, sample_rate, D_STRING_FREQS[noteIndex+2]);
-  
-  copierE.copy();
+
+  //copier.copy();
+  copier2.copy(volume);
   //copierA.copy();
   //copierD.copy();
 }
